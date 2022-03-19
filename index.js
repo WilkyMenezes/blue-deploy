aa3a7781463c9d77c6f98626307966ac80373bb6
WilkyMenezes <willkymenezes@outlook.com>
2022-03-18 22:27:23 -0300

    config deploy

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
